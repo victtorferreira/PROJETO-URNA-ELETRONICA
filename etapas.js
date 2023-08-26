e7e5b374568b274d38661d0d9a6f3f8131e9ea84
victtorferreira <victojhou@gmail.com>
2023-08-26 16:35:49 -0300

    Projeto urna finalizado

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "Newton Cutrim",
+        partido: "ABC",
+        fotos: [{ url: "38111.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "77222",
+        nome: "Roberto Cutrim",
+        partido: "DEFG",
+        fotos: [{ url: "77222.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Daniel Oliveira",
+        partido: "ABC",
+        vice: "Cic",
+        fotos: [
+          { url: "99.jpg", legenda: "Prefeito" },
+          { url: "99_2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Victor Ferreira",
+        partido: "QWERTY",
+        vice: "Zul",
+        fotos: [
+          { url: "84.jpg", legenda: "Prefeito" },
+          { url: "84_2.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
